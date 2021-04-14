4261d143d09aa28f8c5644865a80033969b99ac5
ANTON RODRIGUEZ <71906808+AntonSalimRodriguezAlcantara@users.noreply.github.com>
2021-04-13 21:13:48 -0500

    Add files via upload

---
+++ Introduccion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+float a,b,res;
+
+int main(void)
+{
+        cout<< "\n DIVICION \n";
+        cout<< "\n Ingrese un valor\n";
+        cin>> a;
+
+        cout<<"\n Ingrese el valor 2\n";
+        cin>> b;
+
+        res=a/b;
+
+        cout<<"\n La Divicion es ";
+        cout<< res;
+
+        cout<< endl;
+
+        return 0;
+}
